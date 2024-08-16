13951b4ce290b351886bdf1aa580893d36c0e161
het-tale <ettalebyhasnaa99@gmail.com>
2024-08-16 20:26:59 +0100

    refactor: remove the sqrt method

--- ex01/Program.java
+++ ex01/Program.java
@@ -1,17 +1,6 @@
 import java.util.Scanner;
 
 public class Program {
-    public static double sqrt(int number) {
-        double sqrt = number / 2;
-        double t;
-        if (number < 2)
-            sqrt = number;
-        do {
-            t = sqrt;
-            sqrt = (t + (number / t)) / 2;
-        } while ((t - sqrt) != 0);
-        return sqrt;
-    }
     public static void main(String[] args) {
         int iterationCounter = 1;
         boolean isPrime = true;
@@ -19,16 +8,17 @@ public class Program {
         int number = scan.nextInt();
         if (number < 2) {
             System.err.println("IllegalArgument");
+            scan.close();
             System.exit(-1);
         }
-        for (int i = 2; i <= sqrt(number); i++) {
+        for (int i = 2; i * i <= number; i++) {
             if (number % i == 0) {
                 isPrime = false;
                 break ;
             }
             iterationCounter++;
         }
-        
+        scan.close();
         System.out.print(isPrime+" ");
         System.out.println(iterationCounter);
     }
